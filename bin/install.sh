a903135e337ae1eecb5d2d0b6f16eaee7a281eb5
Rossmiller, Robert <robert.rossmiller@optum.com>
2022-12-12 10:08:28 -0500

    still figuring out cross platform building

--- bin/install.sh
+++ bin/install.sh
@@ -2,28 +2,35 @@
 cd src
 go build
 sudo mv govenv /usr/local/bin
-#! /bin/bash
-read -p "Is your computer using Mac Silicon? (y/n)" macSilicon
-ok=false
 
-if [[ $macSilicon = "y" ]]; then
-    curl -o 
-elif [[ $macSilicon = "n" ]]; then
-    echo "n"
-fi
 
-if [[ ok ]]; then
+# #! /bin/bash
+# read -p "Is your computer using Mac Silicon? (y/n) " macSilicon
+# ok=false
 
-    echo "add this to your .bashrc or .zshrc"
-    echo "
-function venv(){
-    clear
-    govenv $@
-    if [[ $? -eq 0 ]]; then
-        eval \`tail -n 1 ~/.venv/history\`
-        clear
-    fi
-}
-"
+# if [[ $macSilicon = "y" ]]; then
+#     curl -o govenv https://github.com/rrossmiller/venv-manager/releases/download/0.0.1/govenv-arm64
+#     ok=true
+# elif [[ $macSilicon = "n" ]]; then
+#     curl -o govenv https://github.com/rrossmiller/venv-manager/releases/download/0.0.1/govenv-amd64
+#     ok=true
+# fi
 
-fi
+# if [[ ok ]]; then
+#     chmod +x govenv
+#     # sudo mv govenv /usr/local/bin
+#     echo "add this to your .bashrc or .zshrc"
+#     echo "
+# function venv(){
+#     clear
+#     govenv $@
+#     if [[ $? -eq 0 ]]; then
+#         eval \`tail -n 1 ~/.venv/history\`
+#         clear
+#     fi
+# }
+# "
+# else
+#     echo "something went wrong"
+#     echo "ok=$ok, macSilicon=$macSilicon"
+# fi
