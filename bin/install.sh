4703aa9d0dff53c0fa9de8493656a54acbdc30f0
Rossmiller, Robert <robert.rossmiller@optum.com>
2022-12-12 11:48:06 -0500

    instal script

--- bin/install.sh
+++ bin/install.sh
@@ -1,10 +1,39 @@
-#! /bin/bash
-git clone https://github.com/rrossmiller/venv-manager.git ~/.venv-manager && \
-cd ~/.venv-manager/src
-go build && \
-sudo mv govenv /usr/local/bin
-echo "add this to your .bashrc or .zshrc"
-echo "
+#! /bin/zsh
+clear
+# credit where credit's due: https://patorjk.com/software/taag/#p=display&f=Big%20Money-nw&t=venv%20Manager
+echo '
+                                               $$\      $$\                                                             
+                                               $$$\    $$$ |                                                            
+$$\    $$\  $$$$$$\  $$$$$$$\ $$\    $$\       $$$$\  $$$$ | $$$$$$\  $$$$$$$\   $$$$$$\   $$$$$$\   $$$$$$\   $$$$$$\  
+\$$\  $$  |$$  __$$\ $$  __$$\\$$\  $$  |      $$\$$\$$ $$ | \____$$\ $$  __$$\  \____$$\ $$  __$$\ $$  __$$\ $$  __$$\ 
+ \$$\$$  / $$$$$$$$ |$$ |  $$ |\$$\$$  /       $$ \$$$  $$ | $$$$$$$ |$$ |  $$ | $$$$$$$ |$$ /  $$ |$$$$$$$$ |$$ |  \__|
+  \$$$  /  $$   ____|$$ |  $$ | \$$$  /        $$ |\$  /$$ |$$  __$$ |$$ |  $$ |$$  __$$ |$$ |  $$ |$$   ____|$$ |      
+   \$  /   \$$$$$$$\ $$ |  $$ |  \$  /         $$ | \_/ $$ |\$$$$$$$ |$$ |  $$ |\$$$$$$$ |\$$$$$$$ |\$$$$$$$\ $$ |      
+    \_/     \_______|\__|  \__|   \_/          \__|     \__| \_______|\__|  \__| \_______| \____$$ | \_______|\__|      
+                                                                                          $$\   $$ |                    
+                                                                                          \$$$$$$  |                    
+                                                                                           \______/                     
+'
+read -q "?Is your computer using Mac Silicon? (y/n) " macSilicon
+ok=false
+
+if [[ $macSilicon == "y" ]]; then
+    curl -sLJo govenv \
+        -H "Accept: application/octet-stream" \
+        https://github.com/rrossmiller/venv-manager/releases/download/0.0.1/govenv-arm64
+    ok=true
+elif [[ $macSilicon = "n" ]]; then
+    curl -sLJo govenv \
+        -H "Accept: application/octet-stream" \
+        https://github.com/rrossmiller/venv-manager/releases/download/0.0.1/govenv-amd64
+    ok=false
+fi
+
+if [[ ok ]]; then
+    chmod +x govenv
+    sudo mv govenv /usr/local/bin
+    echo "add this to your .bashrc or .zshrc"
+    echo "
 function venv(){
     clear
     govenv $@
@@ -14,34 +43,7 @@ function venv(){
     fi
 }
 "
-
-# #! /bin/bash
-# read -p "Is your computer using Mac Silicon? (y/n) " macSilicon
-# ok=false
-
-# if [[ $macSilicon = "y" ]]; then
-#     curl -o govenv https://github.com/rrossmiller/venv-manager/releases/download/0.0.1/govenv-arm64
-#     ok=true
-# elif [[ $macSilicon = "n" ]]; then
-#     curl -o govenv https://github.com/rrossmiller/venv-manager/releases/download/0.0.1/govenv-amd64
-#     ok=true
-# fi
-
-# if [[ ok ]]; then
-#     chmod +x govenv
-#     # sudo mv govenv /usr/local/bin
-#     echo "add this to your .bashrc or .zshrc"
-#     echo "
-# function venv(){
-#     clear
-#     govenv $@
-#     if [[ $? -eq 0 ]]; then
-#         eval \`tail -n 1 ~/.venv/history\`
-#         clear
-#     fi
-# }
-# "
-# else
-#     echo "something went wrong"
-#     echo "ok=$ok, macSilicon=$macSilicon"
-# fi
+else
+    echo "something went wrong"
+    echo "ok=$ok, macSilicon=$macSilicon"
+fi
