8735723ffc8f3647b78be7ec73f7f80161074724
Rob Rossmiller <rrossmiller3@gmail.com>
2023-10-21 14:57:54 -0400

    update deploy

--- scripts/deploy_rs.sh
+++ scripts/deploy_rs.sh
@@ -12,8 +12,11 @@ if [[ $1 == 'r' ]]; then
     echo "Update version in install.sh to $VERSION"
     echo
     gh release create --generate-notes $VERSION $bin
+    rm $bin
 
 elif [[ $1 == 'd' ]]; then
 # to delete:
     gh release delete $VERSION; git push --delete origin $VERSION
+else
+    echo "must enter 'r' for release or 'd' for delete"
 fi
